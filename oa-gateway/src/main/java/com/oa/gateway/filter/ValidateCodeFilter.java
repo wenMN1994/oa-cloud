46b52232e36b4b5bdd9f7061c407f8ac0bceb422
温文星 <18475536452@163.com>
2022-08-06 13:15:54 +0800

    fix

--- oa-gateway/src/main/java/com/oa/gateway/filter/ValidateCodeFilter.java
+++ oa-gateway/src/main/java/com/oa/gateway/filter/ValidateCodeFilter.java
@@ -3,8 +3,6 @@ package com.oa.gateway.filter;
 import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicReference;
-
-import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -12,6 +10,7 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.oa.common.core.utils.ServletUtils;
 import com.oa.common.core.utils.StringUtils;
